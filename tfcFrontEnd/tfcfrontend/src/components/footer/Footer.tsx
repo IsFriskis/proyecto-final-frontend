169d183e15c63112d2e9f1e33a10618956007bbe
Marcos Martínez <marcos.martinez.fernandez@altia.es>
2023-03-27 09:00:10 +0200

    Little fix

--- tfcFrontEnd/tfcfrontend/src/components/footer/Footer.tsx
+++ tfcFrontEnd/tfcfrontend/src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ export function CustomFooter() {
   return (
     <>
       <footer className="footer">
-        <p>&copy; 2023 Your Company Name. All rights reserved.</p>
+        <p>&copy; 2023 GameZ. All rights reserved.</p>
       </footer>
     </>
   );
